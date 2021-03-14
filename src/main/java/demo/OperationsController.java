aaa412eca72e8c630d80d1d8f75376de2657f5ed
Dima Tchenak <dimatch1@gmail.com>
2021-03-14 15:27:07 +0200

    Added the OperationBoundary and added some logic to the OperationsController

--- src/main/java/demo/OperationsController.java
+++ src/main/java/demo/OperationsController.java
@@ -7,28 +7,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class OperationsController
-{
-	@RequestMapping(
-		path = "/twins/operations",
-		method = RequestMethod.POST,
-		consumes = MediaType.APPLICATION_JSON_VALUE,
-		produces = MediaType.APPLICATION_JSON_VALUE
-	)
-	public Object InvokeOperationOnItem(@RequestBody OperationBoundary operationBoundary)
-	{
-		return "Yese;";
-	}
+public class OperationsController {
+    @RequestMapping(
+            path = "/twins/operations",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object InvokeOperationOnItem(@RequestBody OperationBoundary operationBoundary) {
+//        return "Yese of json object";
+        return  operationBoundary;
+    }
 
 
-	@RequestMapping(
-		path = "/twins/operations/async",
-		method = RequestMethod.POST,
-		consumes = MediaType.APPLICATION_JSON_VALUE,
-		produces = MediaType.APPLICATION_JSON_VALUE
-	)
-	public OperationBoundary aSynchronousOperation(@RequestBody OperationBoundary operationBoundary)
-	{
-		return new OperationBoundary();
-	}
+    @RequestMapping(
+            path = "/twins/operations/async",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public OperationBoundary aSynchronousOperation(@RequestBody OperationBoundary operationBoundary) {
+        return new OperationBoundary(null, "Operation Type", new DigitalItemBoundary(), new UserBoundary());
+    }
 }
