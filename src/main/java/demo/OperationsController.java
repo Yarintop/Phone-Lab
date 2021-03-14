5c21a22c4ee856413fc637971cb0cdefb9038b75
Roee <roeeavir@gmail.com>
2021-03-14 14:57:19 +0200

    Added OperationsController methods (InvokeOperationOnItem and aSynchronousOperation)

--- src/main/java/demo/OperationsController.java
+++ src/main/java/demo/OperationsController.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class OperationsController
 {
 	@RequestMapping(
-		path = "/twins/operations/hello",
-		method = RequestMethod.GET,
+		path = "/twins/operations",
+		method = RequestMethod.POST,
+		consumes = MediaType.APPLICATION_JSON_VALUE,
 		produces = MediaType.APPLICATION_JSON_VALUE
 	)
-	public MessageBoundary hello()
+	public Object InvokeOperationOnItem(@RequestBody OperationBoundary operationBoundary)
 	{
-		return new MessageBoundary("hello world");
+		return "Yese;";
+	}
+
+
+	@RequestMapping(
+		path = "/twins/operations/async",
+		method = RequestMethod.POST,
+		consumes = MediaType.APPLICATION_JSON_VALUE,
+		produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	public OperationBoundary aSynchronousOperation(@RequestBody OperationBoundary operationBoundary)
+	{
+		return new OperationBoundary();
 	}
 }
