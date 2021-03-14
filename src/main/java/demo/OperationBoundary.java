7758cd799557be111fab360f80f3cab0753ab5d7
Dima Tchenak <dimatch1@gmail.com>
2021-03-14 15:26:45 +0200

    Added the OperationBoundary and added some logic to the OperationsController

--- src/main/java/demo/OperationBoundary.java
+++ src/main/java/demo/OperationBoundary.java
@@ -1,9 +1,82 @@
 package demo;
 
-public class OperationBoundary
-{
-    public OperationBoundary()
-    {
-        //TODO
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OperationBoundary {
+
+    private Map<String, String> operationId;
+    private String operationType;
+    private DigitalItemBoundary item;
+    private Date createdTimestamp;
+    private UserBoundary invokedBy;
+    private Map<String, Object> operationAttributes;
+
+
+    public OperationBoundary() {
+        this.operationId = new HashMap<>();
+        this.operationType = "undefined";
+        this.item = new DigitalItemBoundary();
+        this.createdTimestamp = new Date();
+        this.invokedBy = new UserBoundary();
+        this.operationAttributes = new HashMap<>();
+    }
+
+    public OperationBoundary(String id, String operationType, DigitalItemBoundary item, UserBoundary invokedBy) {
+        this();
+        this.operationId.put("space", "2021b.twins"); //TODO: pull it from a resource, make it less hard-coded.
+        this.operationId.put("id", id);
+        this.operationType = operationType;
+        this.item = item;
+        this.invokedBy = invokedBy;
+    }
+
+    public Map<String, String> getOperationId() {
+        return operationId;
+    }
+
+    public void setOperationId(Map<String, String> operationId) {
+        this.operationId = operationId;
+    }
+
+    public String getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(String operationType) {
+        this.operationType = operationType;
+    }
+
+    public DigitalItemBoundary getItem() {
+        return item;
+    }
+
+    public void setItem(DigitalItemBoundary item) {
+        this.item = item;
+    }
+
+    public Date getCreatedTimestamp() {
+        return createdTimestamp;
+    }
+
+    public void setCreatedTimestamp(Date createdTimestamp) {
+        this.createdTimestamp = createdTimestamp;
+    }
+
+    public UserBoundary getInvokedBy() {
+        return invokedBy;
+    }
+
+    public void setInvokedBy(UserBoundary invokedBy) {
+        this.invokedBy = invokedBy;
+    }
+
+    public Map<String, Object> getOperationAttributes() {
+        return operationAttributes;
+    }
+
+    public void setOperationAttributes(Map<String, Object> operationAttributes) {
+        this.operationAttributes = operationAttributes;
     }
 }
