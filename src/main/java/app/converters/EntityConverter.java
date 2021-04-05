44931600c0b40106fb1d60cf0d4ddd152e55dfc5
Dima Tchenak <Dimatch1@gmail.com>
2021-04-05 19:02:53 +0300

    Updated user branch from dev-sprint-3

--- src/main/java/app/converters/EntityConverter.java
+++ src/main/java/app/converters/EntityConverter.java
@@ -3,10 +3,10 @@ package app.converters;
 import app.boundaries.Boundary;
 import app.twins.data.Entity;
 
-public interface EntityConverter <E extends Entity, B extends Boundary> {
-	
-	 E toEntity(B boundaryObject);
-	 
-	 B toBoundary(E entityObject);
-	
+public interface EntityConverter<E extends Entity, B extends Boundary> {
+
+    E toEntity(B boundaryObject);
+
+    B toBoundary(E entityObject);
+
 }
