bc672abb67ae1074393208548f9c10387d643d7a
Liad <Liad.Doh.habari@s.afeka.ac.il>
2021-04-06 14:09:41 +0300

    Enumerate Class UserRole Fixed and updated UserConverter

--- src/main/java/app/converters/UserConverter.java
+++ src/main/java/app/converters/UserConverter.java
@@ -4,15 +4,28 @@ import app.boundaries.UserBoundary;
 import app.twins.data.UserEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 public class UserConverter implements EntityConverter<UserEntity, UserBoundary> {
     @Override
     public UserEntity toEntity(UserBoundary boundaryObject) {
-        return null;
+        UserEntity ue = new UserEntity();
+        if (boundaryObject.getAvatar() != null) {
+            ue.setAvatar(boundaryObject.getAvatar());
+        }
+        if (boundaryObject.getUsername() != null) {
+            ue.setUsername(boundaryObject.getUsername());
+        }
+        return ue;
     }
 
     @Override
     public UserBoundary toBoundary(UserEntity entityObject) {
-        return null;
+        UserBoundary ub =new UserBoundary();
+        ub.setAvatar(entityObject.getAvatar());
+        ub.setUsername(entityObject.getUsername());
+        ub.setUserId((new UserBoundary(entityObject.getEmail(),entityObject.getSpace())).getUserId());
+        return ub;
     }
 }
--- src/main/java/app/twins/data/UserRole.java
+++ src/main/java/app/twins/data/UserRole.java
@@ -3,16 +3,4 @@ package app.twins.data;
 public enum UserRole {
     PLAYER,MANAGER,ADMIN;
 
-    public String getRole(){
-        switch(this){
-            case ADMIN:
-                return "admin";
-            case PLAYER:
-                return "player";
-            case MANAGER:
-                return "manager";
-            default:
-                return null;
-        }
-    }
 }
