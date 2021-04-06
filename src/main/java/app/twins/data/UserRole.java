3a416fd14cb49786bc7c441d4719eca7a12fd263
Liad <Liad.Doh.habari@s.afeka.ac.il>
2021-04-06 13:23:04 +0300

    Enumerate Class UserRole Done

--- src/main/java/app/twins/data/UserRole.java
+++ src/main/java/app/twins/data/UserRole.java
@@ -1,4 +1,18 @@
 package app.twins.data;
 
 public enum UserRole {
+    PLAYER,MANAGER,ADMIN;
+
+    public String getRole(){
+        switch(this){
+            case ADMIN:
+                return "admin";
+            case PLAYER:
+                return "player";
+            case MANAGER:
+                return "manager";
+            default:
+                return null;
+        }
+    }
 }
