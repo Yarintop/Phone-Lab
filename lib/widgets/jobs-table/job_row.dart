89173e04605ba7bf84ba44acda55a648ee554b6e
Dima Tchenak <Dimatch1@gmail.com>
2021-05-26 00:21:28 +0300

    Add navigation functionality + Started to work on the table design

---
+++ lib/widgets/jobs-table/job_row.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+// import 'package:myapp/models/job.dart';
+
+// class JobRow extends StatelessWidget {
+//   final Job job;
+
+//   JobRow({this.job});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: [
+//         Text(job.id),
+//         Text(job.customer),
+//         Text(job.phoneNumber),
+//         Text(job.phoneModel),
+//         Text(job.jobDescription),
+//         Text(job.status),
+//         Text(job.createdTimestamp.toString()),
+//       ],
+//     );
+//   }
+// }
---
+++ lib/widgets/jobs-table/table_header.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+// import 'package:myapp/widgets/jobs-table/cell.dart';
+
+// class TableHeader extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: [
+//         Cell(value: "ID"),
+//         Cell(value: "Customer"),
+//         Cell(value: "Phone Number"),
+//         Cell(value: "Model"),
+//         Cell(value: "Description"),
+//         Cell(value: "Status"),
+//         Cell(value: "Date Added"),
+//       ],
+//     );
+//   }
+// }
